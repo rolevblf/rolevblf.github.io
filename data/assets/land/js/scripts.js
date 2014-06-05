cd81e020461e09cfdc92166d1abba73b0eaf0e06
rolevblf <eavsaha@8LV4WS1.egi.ericsson.com>
2014-06-05 11:10:46 +0530

    First commit

---
+++ data/assets/land/js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('.scroll-background').backstretch([
+      "Theme/assets/land/img/1.jpg"
+    , "Theme/assets/land/img/2.jpg"
+    , "Theme/assets/land/img/3.jpg"
+    ], {duration: 3000, fade: 750});
+
+}
